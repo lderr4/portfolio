bcc58578b87d7bbdc7bee56a397aeffea3d1c843
Lucas Derr <lucasrderr@gmail.com>
2025-05-15 14:58:36 -0700

    finished video

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,13 @@ export default defineConfig({
     server: {
         host: true,
         port: 4321
-    }
+    },
+    vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000, // Check for changes every 1000ms (1s)
+      },
+    },
+  }
 });
