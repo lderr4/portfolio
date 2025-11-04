7642777fbf4233129baaa368d440d4d0e88098c0
Lucas Derr <lucasrderr@gmail.com>
2025-11-03 23:20:34 -0800

    add new blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -34,16 +34,4 @@ export const collections = {
 
 
 
-/*
-title: 4th Down Prediction
-publishDate: 2024-011-29 00:00:00
-img: /assets/4th-down-prediction.png
-img_alt: Pearls of silky soft white cotton, bubble up under vibrant lighting
-description: A live web application that uses machine learning models to predict 4th down outcomes. 
-github_link: https://github.com/lderr4/NFL-4th-Down-Prediction-Algorithm
-project_link: https://4th-down-prediction.com
-tags:
-  - Predictive Modelling
-  - Streamlit
-  - AWS
-*/
+
---
+++ src/content/blogs/transaction-dynamodb.md
@@ -0,0 +1,12 @@
+---
+title: "Building a Transactional System Using DynamoDB"
+publishDate: 2025-11-3 00:00:00
+img: \assets\blogs\transactional-diagram.png
+img_alt: image alt
+github_link: |
+article_link: https://medium.com/@lucasrderr/building-a-transactional-system-using-dynamodb-b1a491552037
+tags:
+  - DynamoDB
+  - Transactional Processing
+---
+
